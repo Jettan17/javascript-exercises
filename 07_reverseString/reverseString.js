597f2482c2bc79523d046ece788eca46aed96b72
Jettan17 <jethrotan2006@gmail.com>
2025-07-11 11:25:24 +0800

    feat: finished up to 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join("");
 };
+
+console.log("rohqor")
 
 // Do not edit below this line
 module.exports = reverseString;
