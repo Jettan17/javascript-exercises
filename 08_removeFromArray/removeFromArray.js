dea0ed5aa1d668c9b866210b6da2deed4c3d5eea
Jettan17 <jethrotan2006@gmail.com>
2025-07-11 13:42:48 +0800

    feat: finished up to 11

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    console.log(arr);
+    for (let element of elements) {
+        if (arr.indexOf(element) > -1) {
+            arr = arr.filter(num => num != element);
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
